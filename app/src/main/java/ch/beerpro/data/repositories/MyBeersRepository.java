5e592283de4a5b85eea47cb3bf56e6d598512d47
Yannick Vogt <yannick.vogt@hotmail.com>
2019-10-29 11:21:50 +0100

    Fixed issue where FridgeItems would not display in MyBeers

--- app/src/main/java/ch/beerpro/data/repositories/MyBeersRepository.java
+++ app/src/main/java/ch/beerpro/data/repositories/MyBeersRepository.java
@@ -23,16 +23,18 @@ import ch.beerpro.domain.models.MyBeerFromRating;
 import ch.beerpro.domain.models.MyBeerFromWishlist;
 import ch.beerpro.domain.models.Rating;
 import ch.beerpro.domain.models.Wish;
+import ch.beerpro.domain.utils.MyBeersContainer;
 
 import static androidx.lifecycle.Transformations.map;
 import static ch.beerpro.domain.utils.LiveDataExtensions.combineLatest;
 
 public class MyBeersRepository {
-    //private static List<MyBeer> getMyBeers(HashMap<String, Beer> allBeers, List<Wish> wishlist, List<Rating> ratings, List<FridgeItem> fridge) {
-    private static List<MyBeer> getMyBeers(Triple<List<Wish>, List<Rating>, HashMap<String, Beer>> input) {
-        List<Wish> wishlist = input.getLeft();
-        List<Rating> ratings = input.getMiddle();
-        HashMap<String, Beer> allBeers = input.getRight();
+
+    private static List<MyBeer> getMyBeers(MyBeersContainer input) {
+        HashMap<String, Beer> allBeers = input.getBeers();
+        List<Wish> wishlist = input.getWishlist();
+        List<Rating> ratings = input.getRatings();
+        List<FridgeItem> fridge = input.getFridge();
 
         ArrayList<MyBeer> result = new ArrayList<>();
         Set<String> beersAlreadyOnTheList = new HashSet<>();
@@ -42,7 +44,6 @@ public class MyBeersRepository {
             beersAlreadyOnTheList.add(beerId);
         }
 
-        /*
         for (FridgeItem fridgeItem : fridge) {
             String beerId = fridgeItem.getBeerId();
 
@@ -51,7 +52,6 @@ public class MyBeersRepository {
                 beersAlreadyOnTheList.add(beerId);
             }
         }
-        */
 
         for (Rating rating : ratings) {
             String beerId = rating.getBeerId();
@@ -68,60 +68,10 @@ public class MyBeersRepository {
         return result;
     }
 
-    /*
-    private static List<MyBeer> getMyBeers(Triple<List<Wish>, List<Rating>, HashMap<String, Beer>> input) {
-        List<Wish> wishlist = input.getLeft();
-        List<Rating> ratings = input.getMiddle();
-        HashMap<String, Beer> beers = input.getRight();
-
-        ArrayList<MyBeer> result = new ArrayList<>();
-        Set<String> beersAlreadyOnTheList = new HashSet<>();
-        for (Wish wish : wishlist) {
-            String beerId = wish.getBeerId();
-            result.add(new MyBeerFromWishlist(wish, beers.get(beerId)));
-            beersAlreadyOnTheList.add(beerId);
-        }
-
-        for (Rating rating : ratings) {
-            String beerId = rating.getBeerId();
-            if (beersAlreadyOnTheList.contains(beerId)) {
-                // if the beer is already on the wish list, don't add it again
-            } else {
-                result.add(new MyBeerFromRating(rating, beers.get(beerId)));
-                // we also don't want to see a rated beer twice
-                beersAlreadyOnTheList.add(beerId);
-            }
-        }
-        Collections.sort(result, (r1, r2) -> r2.getDate().compareTo(r1.getDate()));
-        return result;
-    }
-    */
 
     public LiveData<List<MyBeer>> getMyBeers(LiveData<List<Beer>> allBeers, LiveData<List<Wish>> myWishlist, LiveData<List<FridgeItem>> myFridge, LiveData<List<Rating>> myRatings) {
-        return map(combineLatest(myWishlist, myRatings, map(allBeers, Entity::entitiesById)),
+        return map(combineLatest(myWishlist, myRatings, myFridge, map(allBeers, Entity::entitiesById)),
                 MyBeersRepository::getMyBeers);
     }
 
-    /*
-     public static <X, Y> LiveData<Y> map(
-            @NonNull LiveData<X> source,
-            @NonNull final Function<X, Y> mapFunction) {
-        final MediatorLiveData<Y> result = new MediatorLiveData<>();
-        result.addSource(source, new Observer<X>() {
-            @Override
-            public void onChanged(@Nullable X x) {
-                result.setValue(mapFunction.apply(x));
-            }
-        });
-        return result;
-    }
-    */
-
-    /*
-    public LiveData<List<MyBeer>> getMyBeers(LiveData<List<Beer>> allBeers, LiveData<List<Wish>> myWishlist,
-                                             LiveData<List<Rating>> myRatings) {
-        return map(combineLatest(myWishlist, myRatings, map(allBeers, Entity::entitiesById)),
-                MyBeersRepository::getMyBeers);
-    }
-    */
 }
--- app/src/main/java/ch/beerpro/domain/utils/LiveDataExtensions.java
+++ app/src/main/java/ch/beerpro/domain/utils/LiveDataExtensions.java
@@ -7,7 +7,13 @@ import androidx.lifecycle.MediatorLiveData;
 
 import org.apache.commons.lang3.tuple.Triple;
 
+import java.util.HashMap;
 import java.util.List;
+
+import ch.beerpro.domain.models.Beer;
+import ch.beerpro.domain.models.FridgeItem;
+import ch.beerpro.domain.models.Rating;
+import ch.beerpro.domain.models.Wish;
 
 public class LiveDataExtensions {
 
@@ -90,6 +96,45 @@ public class LiveDataExtensions {
             private void update() {
                 if (lastA != null && lastB != null && lastC != null) {
                     this.setValue(Triple.of(lastA, lastB, lastC));
+                }
+            }
+        };
+    }
+
+    public static LiveData<MyBeersContainer> combineLatest(LiveData<List<Wish>> myWishList, LiveData<List<Rating>> myRatings, LiveData<List<FridgeItem>> myFridge, LiveData<HashMap<String, Beer>> myBeers) {
+        return new MediatorLiveData<MyBeersContainer>() {
+            List<Wish> lastWishlist = null;
+            List<Rating> lastRatings = null;
+            List<FridgeItem> lastFridge = null;
+            HashMap<String, Beer> lastBeers = null;
+
+            {
+                {
+                    addSource(myBeers, (HashMap<String, Beer> a) -> {
+                        lastBeers = a;
+                        update();
+                    });
+
+                    addSource(myWishList, (List<Wish> b) -> {
+                        lastWishlist = b;
+                        update();
+                    });
+
+                    addSource(myRatings, (List<Rating> c) -> {
+                        lastRatings = c;
+                        update();
+                    });
+
+                    addSource(myFridge, (List<FridgeItem> d) -> {
+                        lastFridge = d;
+                        update();
+                    });
+                }
+            }
+
+            private void update() {
+                if (lastBeers != null && lastWishlist != null && lastRatings != null && lastFridge != null) {
+                    this.setValue(new MyBeersContainer(lastBeers, lastWishlist, lastRatings, lastFridge));
                 }
             }
         };
